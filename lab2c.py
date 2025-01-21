0e02ceb48a0ab8d3f76eb67748681723476a8dc5
Ali Ahmad Faiz Ahmad <afaiz-ahmad@myseneca.ca>
2025-01-20 22:39:38 -0500

    Added lab2c

---
+++ lab2c.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+'''
+This program will prompt the user for inout data
+Author: Ali Ahmad Faiz Ahmad (Afaiz-ahmad)
+'''
+
+import sys
+
+name = sys.argv[1]
+age = int(sys.argv[2])
+
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')
+
+
+
+
