d17b67cba510c262a757a1baa5be7139971764c3
Ali Ahmad Faiz Ahmad <afaiz-ahmad@myseneca.ca>
2025-01-20 22:12:55 -0500

    Added lab2b

---
+++ lab2b.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+'''
+This program will prompt the user for inout data
+Author: Ali Ahmad Faiz Ahmad (Afaiz-ahmad)
+'''
+
+name = input("Name: ")
+age = input("Age: ")
+
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')
+
+
+
+
