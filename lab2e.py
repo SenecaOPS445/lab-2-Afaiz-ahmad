501404232924b675016080843e8de445d8753654
Ali Ahmad Faiz Ahmad <afaiz-ahmad@myseneca.ca>
2025-01-21 21:48:48 -0500

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3 
+
+
+# Author: Ali Ahmad Faiz Ahmad 
+# Author ID: Afaiz-ahmad
+# Date Created: 2025/01/21
+
+timer = 10 
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print('blast off!')
+
